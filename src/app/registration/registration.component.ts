7785f75e283274b719dd9244ac5fb35afee23d0a
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-07 23:00:11 +0530

    reg validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  name=""
+  address=""
+  email=""
+  phno=""
+  username=""
+  password=""
+
+
+  constructor(private api:ApiService) {}
+
+  regValues=()=>
+  {
+    let data:any={"name":this.name,"address":this.address,"email":this.email,"phno":this.phno,"username":this.username,"password":this.password}
+    console.log(data)
+    this.api.addproduct(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if(response.status=="success")
+        {
+          alert("added")
+          this.name="",this.address="",this.email="",this.phno="",this.username="",this.password=""
+        }
+        else
+        {
+          alert("error")
+        }
+      }
+    )
+  }
 
 }
--- src/app/userlogin/userlogin.component.ts
+++ src/app/userlogin/userlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userlogin',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./userlogin.component.css']
 })
 export class UserloginComponent {
+
+
+  username=""
+  password=""
+constructor(private route:Router){}
+  loginValues=()=>
+  {
+    let data:any=
+    {
+      "username":this.username,"password":this.password
+    }
+    console.log(data)
+
+    if (this.username=="admin" && this.password=="1234") {
+this.route.navigate(["/add"])      
+    } else {
+      alert("invalid credentials")
+      
+    }
+  }
 
 }
