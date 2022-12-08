75d44b3a2fd899d913a4d27502f63c0193882e13
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-08 09:42:13 +0530

    userview crct

--- src/app/userview/userview.component.ts
+++ src/app/userview/userview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-userview',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class UserviewComponent {
 
+
+  constructor(private api:ApiService)
+  {
+    api.viewproduct().subscribe(
+      (response)=>
+      {
+        this.data=response;
+      }
+    )
+  }
+data:any=[]
 }
